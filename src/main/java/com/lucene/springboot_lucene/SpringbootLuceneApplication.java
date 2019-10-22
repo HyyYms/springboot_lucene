1ac0069678e14a45b09c05b57d5a1a2395a5bb8d
YaYaHuang <570911275@qq.com>
2019-10-22 09:46:07 +0800

    Initial commit

---
+++ src/main/java/com/lucene/springboot_lucene/SpringbootLuceneApplication.java
@@ -0,0 +1,13 @@
+package com.lucene.springboot_lucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLuceneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLuceneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucene/springboot_lucene/SpringbootLuceneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucene.springboot_lucene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLuceneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
